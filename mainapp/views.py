6096722bb2f7dd1ce9aceb7a65252fc363890278
Marsel <marselmihajlov@yandex.ru>
2022-11-06 18:18:00 +0300

    3_less

--- mainapp/views.py
+++ mainapp/views.py
@@ -16,6 +16,34 @@ class CoursesPageView(TemplateView):
 class ContactsPageView(TemplateView):
     template_name = 'mainapp/contacts.html'
 
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+
+        context_data['contacts'] = [
+            {
+                'map': 'https://yandex.ru/map-widget/v1/-/CCUAZHcrhA',
+                'city': 'Санкт‑Петербург',
+                'phone': '+7-999-11-11111',
+                'email': 'geeklab@spb.ru',
+                'adress': 'территория Петропавловская крепость, 3Ж'
+            },
+            {
+                'map': 'https://yandex.ru/map-widget/v1/-/CCUAZHX3xB',
+                'city': 'Казань',
+                'phone': '+7-999-22-22222',
+                'email': 'geeklab@kz.ru',
+                'adress': 'территория Кремль, 11, Казань, Республика Татарстан, Россия'
+            },
+            {
+                'map': 'https://yandex.ru/map-widget/v1/-/CCUAZHh9kD',
+                'city': 'Москва',
+                'phone': '+7-999-33-33333',
+                'email': 'geeklab@msk.ru',
+                'adress': 'Красная площадь, 7, Москва, Россия'
+            }
+        ]
+        return context_data
+
 
 class DocSitePageView(TemplateView):
     template_name = 'mainapp/doc_site.html'
